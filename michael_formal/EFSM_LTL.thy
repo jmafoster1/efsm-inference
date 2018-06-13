e7a36574746f346c894d669aa06b7ed260b19066
Michael Foster <jmafoster1@sheffield.ac.uk>
2018-06-13 11:57:29 +0100

    First subgoal proved

--- michael_formal/EFSM_LTL.thy
+++ michael_formal/EFSM_LTL.thy
@@ -1,19 +1,19 @@
 theory EFSM_LTL
-imports Contexts EFSM Filesystem
+imports EFSM Filesystem
 begin
 
-abbreviation neXt :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> event \<Rightarrow> (statename \<Rightarrow> event \<Rightarrow> registers \<Rightarrow> outputs \<Rightarrow> bool) \<Rightarrow> bool" where
-  "neXt e s r h f \<equiv> (case step e s r (fst h) (snd h) of
-      None \<Rightarrow> False |
-      Some (s', o', r') \<Rightarrow> f s' h r' o'
-    )"
+definition step :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> label \<Rightarrow> inputs \<Rightarrow> (statename \<times> outputs \<times> registers)" where
+"step e s r l i \<equiv>
+  case (possible_steps e s r l i) of
+    [(s',t)] \<Rightarrow> (s', (apply_outputs (Outputs t) (join_ir i r 1)), (apply_updates (Updates t) (join_ir i r 1) r)) |
+    _ \<Rightarrow> (0, [], r)"
 
-primrec globally :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> outputs \<Rightarrow> trace \<Rightarrow> (statename \<Rightarrow> event \<Rightarrow> registers \<Rightarrow> outputs \<Rightarrow> bool) \<Rightarrow> bool" where
-  "globally e s r p [] f = f s ('''', []) r p" |
-  "globally e s r p (h#t) f = (case step e s r (fst h) (snd h) of
-    None \<Rightarrow> False |
-    Some (s', o', r') \<Rightarrow> conj (f s h r p) (globally e s' r' o' t f)
-  )"
+abbreviation neXt :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> event \<Rightarrow> ((statename \<times> outputs \<times> registers) \<Rightarrow> event \<Rightarrow> bool) \<Rightarrow> bool" where
+  "neXt e s r h f \<equiv> f (step e s r (fst h) (snd h)) h"
+
+primrec globally :: "efsm \<Rightarrow> (statename \<times> outputs \<times> registers) \<Rightarrow> trace \<Rightarrow> (statename \<Rightarrow> event \<Rightarrow> registers \<Rightarrow> outputs \<Rightarrow> bool) \<Rightarrow> bool" where
+  "globally e spr [] f = f (fst spr) ('''', []) (snd (snd spr)) (fst (snd spr))" |
+  "globally e spr (h#t) f = conj (f (fst spr) h (snd (snd spr)) (fst (snd spr))) (globally e (step e (fst spr) (snd (snd spr)) (fst h) (snd h)) t f)"
 
 (*
 noChangeOwner: THEOREM filesystem |- FORALL (user : UID): G(cfstate /= NULL_STATE) => 
@@ -25,33 +25,64 @@ G(
   );
 *)
 
-lemma state2: "e \<noteq> (''read'', []) \<Longrightarrow> e \<noteq> (''logout'', []) \<Longrightarrow> \<not> (fst e = ''write'' \<and> length (snd e) = 1) \<Longrightarrow> step filesystem 2 r (fst e) (snd e) = None"
-  apply (simp add: fs_simp step_def)
-  apply safe
-                      apply simp_all
-         apply (metis prod.collapse)
-        apply (metis prod.collapse)
-       apply (metis prod.collapse)
-      apply (metis prod.collapse)
-     apply (metis prod.collapse)
-    apply (metis prod.collapse)
-   apply (metis prod.collapse)
-  by (metis prod.collapse)
-
-lemma read_not_none [simp]: "step filesystem 2 r ''read'' [] \<noteq> None"
+lemma read_not_none [simp]: "\<forall>r'. step filesystem 2 r ''read'' [] \<noteq> (0, [], r')"
   by (simp add: fs_simp step_def)
 
-lemma read_no_side_effects [simp]: "step filesystem 2 r ''read'' [] = Some (aa, aaa, b) \<Longrightarrow> b = r \<and> aa = 2"
+lemma read_no_side_effects [simp]: "\<exists>p. step filesystem 2 r ''read'' [] = (2, p, r)"
   apply (simp add: fs_simp step_def)
   apply (case_tac "r ''r1'' = r ''r3''")
    apply simp
   apply (rule ext)
   by auto
 
- lemma "all (t) (\<lambda>e. e = (''read'', []) \<or> e = (''logout'', []) \<or> (fst e = ''write'' \<and> length (snd e) = 1)) \<Longrightarrow> 
- globally filesystem 2 r [] t (\<lambda>s e r p. (s = 2 \<and> fst e = ''write'' \<and> r ''r1'' = 1) \<longrightarrow>
-  globally filesystem s' r' [] t (\<lambda>s e r p. (s = 2 \<and> fst e = ''read'' \<and> r ''r1'' \<noteq> 1)) \<longrightarrow>
-    neXt filesystem s'' r'' (hd t') (\<lambda>s e r p. p = [0])
+lemma read_no_side_effects2 [simp]: "step filesystem 2 r ''read'' [] = a \<Longrightarrow> snd (snd a) = r \<and> fst a = 2"
+  apply (simp add: step_def)
+  apply (simp add: fs_simp)
+  apply (case_tac "r ''r1'' = r ''r3''")
+   apply simp
+   apply safe[1]
+    apply (rule ext, simp, simp)
+  apply simp
+  apply safe
+  by (rule ext, simp, simp)
+
+lemma truefun : "\<forall> e spr. globally e spr t (\<lambda>s e r' p. True)"
+proof (induction t)
+  case Nil
+  then show ?case by simp
+next
+  case (Cons a t)
+  then show ?case
+    apply simp
+    by (simp add: Cons.IH)
+qed
+
+lemma read_denied: "r ''r1'' \<noteq> 1 \<Longrightarrow> r ''r3'' = 1 \<Longrightarrow> EFSM_LTL.step filesystem 2 r ''read'' [] = (2, [0], r)"
+  apply (simp add: fs_simp step_def)
+  by (rule ext, simp)
+
+lemma read_success: "r ''r1''= r ''r3'' \<Longrightarrow> EFSM_LTL.step filesystem 2 r ''read'' [] = (2, [r ''r2''], r)"
+  apply (simp add: fs_simp step_def)
+  by (rule ext, simp)
+
+lemma read_denied1: "r ''r1'' \<noteq> r ''r3'' \<Longrightarrow> EFSM_LTL.step filesystem 2 r ''read'' [] = (2, [0], r)"
+  apply (simp add: fs_simp step_def)
+  by (rule ext, simp)
+
+lemma aux1: "r ''r1'' = r ''r3'' \<Longrightarrow>
+    (\<lambda>x. if x = ''r1'' then aval (snd (''r1'', V ''r1'')) r else apply_updates [(''r2'', V ''r2''), (''r3'', V ''r3'')] r r x) = r"
+  by (rule ext, simp)
+
+lemma read_step: "EFSM_LTL.step filesystem 2 r ''read'' [] = (2, [r ''r2''], r) \<or>
+EFSM_LTL.step filesystem 2 r ''read'' [] = (2, [0], r)"
+  apply (case_tac "r ''r1'' = r ''r3''")
+   apply (simp add: read_success)
+  by (simp add: read_denied1)
+
+lemma "all (t) (\<lambda>e. e = (''read'', []) \<or> e = (''logout'', []) \<or> (fst e = ''write'' \<and> length (snd e) = 1)) \<Longrightarrow> 
+ \<forall> outs r. globally filesystem (2, outs, r) t (\<lambda>s e r' p. (s = 2 \<and> fst e = ''write'' \<and> r' ''r1'' = 1) \<longrightarrow>
+  globally filesystem (2, outs, r) t (\<lambda>s e r' p. (s = 2 \<and> fst e = ''read'' \<and> r' ''r1'' \<noteq> 1)) \<longrightarrow>
+    neXt filesystem s' r'' (hd t') (\<lambda>spr e. (fst (snd spr)) = [0])
 )"
 proof (induction t)
   case Nil
@@ -59,10 +90,27 @@ proof (induction t)
 next
   case (Cons a t')
   then show ?case
-    apply (case_tac "a = (''read'', [])")
+    apply (case_tac "a = (''read'', [])", simp)
+     apply (rule allI)
+     apply (case_tac "(EFSM_LTL.step filesystem 2 r ''read'' []) = (2, [0], r)", simp)
+      apply (case_tac "r ''r1'' = 1")
+       apply (simp add: truefun)
+      apply simp
+     apply (case_tac "r ''r1'' = 1")
+      apply (simp add: truefun)
      apply simp
+     apply (case_tac "r ''r3'' = 1")
+      apply (simp add: read_denied)
+    apply (case_tac "r ''r1'' = r ''r3''")
+      apply (simp add: read_success)
+     apply (simp add: read_denied1)
+
+     
+    
 
 
+
+     
 
 
 qed
