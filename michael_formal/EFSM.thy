0b5019a0d42d4b871d055ddef9a418b1b35affc1
Ramsay Taylor <r.g.taylor@sheffield.ac.uk>
2018-06-21 10:43:19 +0100

    Changed definition of observe_trace to just be a map over observe_all

--- michael_formal/EFSM.thy
+++ michael_formal/EFSM.thy
@@ -30,14 +30,6 @@ definition step :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Right
     [(s',t)] \<Rightarrow> Some (s', (apply_outputs (Outputs t) (join_ir i r)), (apply_updates (Updates t) (join_ir i r) r)) |
     _ \<Rightarrow> None"
 
-primrec observe_trace :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> trace \<Rightarrow> observation" where
-  "observe_trace _ _ _ [] = []" |
-  "observe_trace e s r (h#t) = 
-    (case (step e s r (fst h) (snd h)) of
-      Some (s', outputs, updated) \<Rightarrow> (outputs#(observe_trace e s' updated t)) |
-      _ \<Rightarrow> []
-    )"
-
 primrec observe_all :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> trace \<Rightarrow> (statename \<times> outputs \<times> registers) list" where
   "observe_all _ _ _ [] = []" |
   "observe_all e s r (h#t) = 
@@ -45,6 +37,9 @@ primrec observe_all :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<R
       (Some (s', outputs, updated)) \<Rightarrow> (((s', outputs, updated)#(observe_all e s' updated t))) |
       _ \<Rightarrow> []
     )"
+
+abbreviation observe_trace :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> trace \<Rightarrow> observation" where
+  "observe_trace e s r t \<equiv> map (\<lambda>(x,y,z). y) (observe_all e s r t)"
 
 definition equiv :: "efsm \<Rightarrow> efsm \<Rightarrow> trace \<Rightarrow> bool" where
   "equiv e1 e2 t \<equiv> ((observe_trace e1 (s0 e1) <> t) = (observe_trace e2 (s0 e2) <> t))"
@@ -58,14 +53,6 @@ lemma equiv_trans: "equiv e1 e2 t \<and> equiv e2 e3 t \<longrightarrow> equiv e
 
 lemma equiv_idem: "equiv e1 e1 t"
   by (simp add: equiv_def)
-
-primrec valid_trace_1 :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> trace \<Rightarrow> bool" where
-  "valid_trace_1 _ _ _ [] = True" |
-  "valid_trace_1 e s r (h#t) = 
-    (case (step e s r (fst h) (snd h)) of
-      Some (s', _, updated) \<Rightarrow> (valid_trace_1 e s' updated t) |
-      _ \<Rightarrow> False
-    )"
 
 abbreviation valid_trace :: "efsm \<Rightarrow> trace \<Rightarrow> bool" where
   "valid_trace e t \<equiv> (length t = length (observe_all e (s0 e) <> t))"
@@ -86,4 +73,24 @@ definition can_take :: "transition \<Rightarrow> transition \<Rightarrow> bool" 
 primrec find_match :: "transition \<Rightarrow> destination list \<Rightarrow> destination option" where
   "find_match _ [] = None" |
   "find_match t (h#tail) = (if (can_take t (snd h)) then (Some h) else (find_match t tail))"
+
+lemma valid_unit_trace: "step e (s0 e) <> l i = Some (s',outs,r) \<Longrightarrow> valid_trace e [(l,i)]"
+  by auto
+
+lemma different_observation_techniques: 
+  shows "length(observe_all e s r t) = length(observe_trace e s r t)"
+  by simp
+
+lemma valid_extension:
+  fixes e and t and l and i and s and r
+  assumes "valid_trace e t" 
+  and "observe_all e (s0 e) <> t = (oo @ [(s, outs, r)])"
+  and "step e s r l i = Some (s'',outs',r')"
+  shows "valid_trace e (t @ [(l,i)])"
+  apply simp
+  apply (simp only: observe_all_def)
+
+  
+  
+
 end
