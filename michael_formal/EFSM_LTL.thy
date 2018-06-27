3e924451b35bcb3156a76b4a1506afea5ad12d80
Michael Foster <jmafoster1@sheffield.ac.uk>
2018-06-27 11:57:14 +0100

    New LTL phrasing

--- michael_formal/EFSM_LTL.thy
+++ michael_formal/EFSM_LTL.thy
@@ -2,22 +2,18 @@ theory EFSM_LTL
 imports EFSM Filesystem "HOL-Library.Sublist"
 begin
 
-definition step :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> label \<Rightarrow> inputs \<Rightarrow> (statename \<times> outputs \<times> registers)" where
-"step e s r l i \<equiv>
-  case (possible_steps e s r l i) of
-    [(s',t)] \<Rightarrow> (s', (apply_outputs (Outputs t) (join_ir i r)), (apply_updates (Updates t) (join_ir i r) r)) |
+definition step :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> event \<Rightarrow> (statename \<times> outputs \<times> registers)" where
+"step e s r ev \<equiv>
+  case (possible_steps e s r (fst ev) (snd ev)) of
+    [(s',t)] \<Rightarrow> (s', (apply_outputs (Outputs t) (join_ir (snd ev) r)), (apply_updates (Updates t) (join_ir (snd ev) r) r)) |
     _ \<Rightarrow> (0, [], r)"
 
-primrec step_trace :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> trace \<Rightarrow> (statename \<times> outputs \<times> registers) list" where
-"step_trace e s r [] = []" |
-"step_trace e s r (h#t) = (let (s', p, r') = step e s r (fst h) (snd h) in (s', p, r')#(step_trace e s' r' t))"
-
 abbreviation neXt :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> event \<Rightarrow> ((statename \<times> outputs \<times> registers) \<Rightarrow> event \<Rightarrow> bool) \<Rightarrow> bool" where
-  "neXt e s r v f \<equiv> f (step e s r (fst v) (snd v)) v"
+  "neXt e s r v f \<equiv> f (step e s r v) v"
 
 primrec globally :: "efsm \<Rightarrow> (statename \<times> outputs \<times> registers) \<Rightarrow> trace \<Rightarrow> ((statename \<times> outputs \<times> registers) \<Rightarrow> event \<Rightarrow> bool) \<Rightarrow> bool" where
   "globally e spr [] f = (\<exists>e. f spr e)" |
-  "globally e spr (h#t) f = conj (f spr h) (globally e (step e (fst spr) (snd (snd spr)) (fst h) (snd h)) t f)"
+  "globally e spr (h#t) f = conj (f spr h) (globally e (step e (fst spr) (snd (snd spr)) h) t f)"
 
 primrec globally2 :: "(statename \<times> event \<times> registers \<times> outputs) list \<Rightarrow> ((statename \<times> event \<times> registers \<times> outputs) \<Rightarrow> bool) \<Rightarrow> bool" where
   "globally2 [] _ = True" |
@@ -29,12 +25,6 @@ lemma globally_empty: "globally e spr [t] f \<Longrightarrow> globally e spr [] 
     apply (rule_tac x="snd t" in exI)
   by simp
 
-lemma login_1: "fst a = ''login'' \<and> length (snd a) = Suc 0 \<Longrightarrow> step filesystem 1 r ''login'' (snd a) = (2, [], (\<lambda>x. if x = ''r1'' then hd (snd a) else r x))"
-  apply (simp add: fs_simp step_def)
-  apply (rule ext)
-  apply simp
-  by (metis Suc_length_conv i1 index2state.simps(2) list.sel(1))
-
 lemma notZero: "globally filesystem (0, [], r) t (\<lambda>(s, a) a. s \<noteq> 0) \<Longrightarrow> False"
 proof (induction t)
 case Nil
@@ -43,9 +33,6 @@ next
   case (Cons a t)
   then show ?case by simp
 qed
-
-lemma login_2: "step filesystem 2 r ''login'' x = (0, [], r)"
-  by (simp add: fs_simp step_def)
 
 lemma contra: "\<not> globally e (s, outs, r) [] f \<Longrightarrow> \<not> globally e (s, outs, r) x f"
 proof (induction x)
@@ -77,121 +64,7 @@ next
     (* try *)
 qed
 
-lemma login_x: "step filesystem 1 r ''login'' [x] = (2, [], (\<lambda>i. if i = ''r1'' then x else r i))"
-  apply (simp add: fs_simp step_def)
-  apply (rule ext)
-  by simp
-
-lemma read_1: "step filesystem 1 r ''read'' [] = (0, [], r)"
-  by (simp add: fs_simp step_def)
-
-lemma logout_1: "step filesystem 1 r ''logout'' [] = (0, [], r)"
-  by (simp add: fs_simp step_def)
-
-lemma write_1: "step filesystem 1 r ''write'' (snd h) = (0, [], r)"
-  by (simp add: fs_simp step_def)
-
-lemma noOtherInputs: "h \<noteq> (''read'', []) \<Longrightarrow>
-    h \<noteq> (''logout'', []) \<Longrightarrow>
-    \<not> (fst h = ''write'' \<and> length (snd h) = 1) \<Longrightarrow>
-    \<not> (fst h = ''login'' \<and> length (snd h) = 1) \<Longrightarrow> \<forall>s r. step filesystem s r (fst h) (snd h) = (0, [], r)"
-  apply (simp add: fs_simp step_def)
-  by (smt prod.collapse)
-
-lemma noOtherStates: "s \<noteq> 1 \<Longrightarrow>
-                s \<noteq> 2 \<Longrightarrow>
-                s \<noteq> 0 \<Longrightarrow> \<forall>r i l. step filesystem s r l i = (0, [], r)"
-  by (simp add: fs_simp step_def)
-
-lemma prependLogin: "\<forall>h t. (globally filesystem (s,outs,r) (h#t) (\<lambda>(s, p, r) e. s \<noteq> 0) \<longrightarrow> fst x = ''login'' \<and> length (snd x) = 1 \<longrightarrow>  \<not> globally filesystem (s,outs,r) (x#h#t) (\<lambda>(s, p, r) e. s \<noteq> 0))"
-  apply (rule allI, rule allI)
-  apply simp
-  apply (case_tac "s=0")
-   apply simp
-  apply (case_tac "s=1")
-   apply (simp add: login_x)
-   apply (case_tac "h = (''read'', [])")
-    apply (simp add: read_1 contra)
-   apply (case_tac "h = (''logout'', [])")
-    apply (simp add: logout_1 contra)
-   apply (case_tac "fst h = ''write'' \<and> length (snd h) = 1")
-    apply (simp add: write_1 contra)
-   apply (case_tac "fst h = ''login'' \<and> length (snd h) = 1")
-    apply (simp add: login_1 login_2 contra)
-   apply (simp add: noOtherInputs contra)
-  apply (case_tac "s=2")
-   apply safe
-           apply (simp add: login_2)
-  by (simp add: noOtherStates)
-
-lemma loginNot1: "s \<noteq> 1 \<Longrightarrow>step filesystem s r ''login'' (snd x) = (0, [], r)"
-  by (simp add: fs_simp step_def)
-
-lemma nonZeroFun: "(\<lambda>a. case a of (s, a) \<Rightarrow> case a of (p, r) \<Rightarrow> \<lambda>e. s \<noteq> 0) = (\<lambda>(s, p, r) e. s \<noteq> 0)"
-  by auto
-
-lemma notLogin_1:  "\<not> (fst x = ''login'' \<and> length (snd x) = 1) \<Longrightarrow> step filesystem 1 r (fst x) (snd x) = (0, [], r)"
-  by (simp add: fs_simp step_def)
-
-lemma read_2_neq: "r ''r1'' \<noteq> r ''r3'' \<Longrightarrow> step filesystem 2 r ''read'' [] = (2, [0], r)"
-  apply (simp add: fs_simp step_def)
-  apply (rule ext)
-  by simp
-
-lemma read_2_eq: "r ''r1'' = r ''r3'' \<longrightarrow> step filesystem 2 r ''read'' [] = (2, [r ''r2''], r)"
-  apply (simp add: fs_simp step_def)
-  apply safe
-  apply (rule ext)
-  by simp
-
-lemma logout_2: "step filesystem 2 r ''logout'' [] = (1, [], r)"
-  apply (simp add: fs_simp step_def)
-  apply (rule ext)
-  by simp
-
-lemma write_2_eq: "fst x = ''write'' \<and> length (snd x) = Suc 0 \<Longrightarrow>
-    r ''r1'' = r ''r3'' \<Longrightarrow>
-    step filesystem 2 r ''write'' (snd x) = (2, [], (\<lambda>i. if i = ''r2'' then hd(snd x) else (if i = ''r3'' then r ''r1'' else r i)))"
-  apply (simp add: step_def fs_simp)
-   apply (rule ext)
-   apply simp
-  by (metis Suc_length_conv i1 index2state.simps(2) list.sel(1))
-
-lemma write_2_neq: "fst x = ''write'' \<and> length (snd x) = Suc 0 \<Longrightarrow>
-    r ''r1'' \<noteq> r ''r3'' \<Longrightarrow>
-    step filesystem 2 r ''write'' (snd x) = (2, [0], r)"
-  by (simp add: fs_simp step_def)
-
-lemma noMoreInputs_2: "x \<noteq> (''read'', []) \<Longrightarrow>
-    x \<noteq> (''logout'', []) \<Longrightarrow>
-    \<not> (fst x = ''write'' \<and> length (snd x) = 1) \<Longrightarrow> step filesystem 2 r (fst x) (snd x) = (0, [], r)"
-  apply (simp add: fs_simp step_def)
-  by (metis prod.collapse)
-
-lemma step_0: "step filesystem 0 r i l = (0, [], r)"
-  by (simp add: fs_simp step_def)
-
-lemma sink: "globally filesystem (0, [], <>) xs (\<lambda>(s', p', r') e'. s' = 2 \<and> fst e' = ''read'' \<and> r' ''r1'' \<noteq> 0 \<longrightarrow> fst (snd (EFSM_LTL.step filesystem 2 r' ''read'' (snd e'))) = [0])"
-proof (induction xs)
-  case Nil
-  then show ?case by simp
-next
-  case (Cons a xs)
-  then show ?case by (simp add: step_0)
-qed
-
-lemma aux1: "globally filesystem (1, outs, <>) xs (\<lambda>a. case a of (s, a) \<Rightarrow> \<lambda>a. s \<noteq> 0) \<Longrightarrow>  xs = a # list \<Longrightarrow>
-              \<not> globally filesystem (2, [], \<lambda>i. if i = ''r1'' then hd (snd x) else 0) xs (\<lambda>(s, a) a. s \<noteq> 0)"
-  apply simp
-  apply (case_tac "fst a = ''login'' \<and> length (snd a) = 1")
-   apply (simp add: login_1 login_2 contra)
-  by (simp add: notLogin_1 contra)
-
-lemma empty_equiv: "(\<lambda>i. if i = ''r1'' then 0 else 0) = <>"
-  apply (rule ext)
-  by (simp add: null_state_def)
-
-lemma true_fun: "globally2 t (\<lambda>x. True) = True"
+lemma true_fun[simp]: "globally2 t (\<lambda>x. True) = True"
 proof (induction t)
   case Nil
   then show ?case by simp
@@ -216,16 +89,6 @@ next
 qed
 
 lemma prefix_zs: "\<exists>zs. t' = t @ zs \<longrightarrow> globally2 t' f = globally2 (t@zs) f"
-  by simp
-
-lemma test1: "\<forall>t. \<exists>zs t'. t = t' @ zs"
-  apply (rule allI)
-  apply (case_tac t)
-   apply (rule_tac x="[]" in exI)
-   apply (rule_tac x="[]" in exI)
-   apply simp
-  apply (rule_tac x="list" in exI)
-  apply (rule_tac x="[a]" in exI)
   by simp
 
 lemma globally2_prefix: "globally2 (t@t') f \<Longrightarrow> globally2 t f"
@@ -303,7 +166,6 @@ next
   then show ?case by simp
 qed
 
-
 lemma globally_extra_element: "globally2 t f \<Longrightarrow> globally2 (t@t') f = globally2 t' f"
 proof (induct t')
   case Nil
@@ -321,20 +183,31 @@ qed
 
 (* primrec observe_temp :: "efsm \<Rightarrow> statename \<Rightarrow> registers \<Rightarrow> trace \<Rightarrow> (statename \<times> event \<times> registers \<times> outputs) list" where *)
 
-lemma "prefix (observe_temp filesystem 1 <> x) (observe_temp filesystem 1 <> (x @ [a]))"
-  apply (simp add: prefix_def)
-proof (induction x rule: rev_induct)
-  case Nil
+lemma filesystem_prefix_2: "prefix (observe_temp filesystem 1 <> x) (observe_temp filesystem 1 <> (x @ [a]))"
+  by (simp add: observe_prefix)
+
+
+abbreviation "fsg \<equiv> globally filesystem"
+
+lemma "globally2 (observe_temp filesystem s r (e#t)) (\<lambda>(s, e, r, p). s \<noteq> 0) \<longrightarrow>
+globally2 (observe_temp filesystem s r (e#t)) (\<lambda>(st, ev, re, p). (fst e = ''write'' \<and> r ''r1'' = 0 \<and> snd e \<noteq> [0]) \<longrightarrow>
+    globally2 (observe_temp filesystem s' r' t) (\<lambda>(s', e', r', p'). (s' = 2 \<and> fst e' = ''read'' \<and> r' ''r1'' \<noteq> 0)\<longrightarrow>
+    (fst (snd (step filesystem s' r' e'))) = [0]))"
+proof (induction "t" rule: rev_induct)
+case Nil
   then show ?case by simp
 next
   case (snoc a x)
   then show ?case
     apply simp
-    apply (rule_tac x="let (s', e, r', o') = observe_temp filesystem 1 <> (x @ [a]) in [observe_temp filesystem s' r' [a]]" in exI)
+    apply (cases "EFSM.step filesystem s r (fst e) (snd e)")
+     apply simp
+    apply simp
+    apply (case_tac "aa")
+    apply simp
+    
 
 qed
-
-abbreviation "fsg \<equiv> globally filesystem"
 
 lemma "globally2 (observe_temp filesystem 1 <> t) (\<lambda>(s, e, r, p). s \<noteq> 0) \<longrightarrow>
 globally2 (observe_temp filesystem 1 <> t) (\<lambda>(s, e, r, p). (fst e = ''write'' \<and> r ''r1'' = 0 \<and> snd e \<noteq> [0]) \<longrightarrow>
@@ -349,10 +222,7 @@ next
     apply simp
     apply (cases "globally2 (observe_temp filesystem 1 <> (x @ [a])) (\<lambda>a. case a of (s, a) \<Rightarrow> s \<noteq> 0)")
      apply (simp add: prop_aux_2)
-    apply (simp add: globally_extra_element)
-
-
-    
+     apply (simp add: globally_extra_element filesystem_prefix_2)
 qed
 
 lemma "fsg (1,outs,<>) t (\<lambda>(s, p, r) e. s \<noteq> 0) \<longrightarrow>
