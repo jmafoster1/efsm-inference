cd4033c113e10d0888d5ceb79c1b3b06300dee84
Michael Foster <jmafoster1@sheffield.ac.uk>
2019-01-18 14:48:44 +0000

    Dirties in a safe file

--- src/Dirties.scala
+++ src/Dirties.scala
@@ -1,1 +1,16 @@
+def scalaChoiceAux(ta: Transition.transition_ext[Unit],
+                 t: Transition.transition_ext[Unit]):
+      Boolean
+  =
+false
 
+def scalaDirectlySubsumes(a: FSet.fset[((Nat.nat, Nat.nat), Transition.transition_ext[Unit])],
+                       b: FSet.fset[((Nat.nat, Nat.nat), Transition.transition_ext[Unit])], c: Nat.nat,
+                       d: Transition.transition_ext[Unit], e: Transition.transition_ext[Unit]):
+  Boolean
+=
+false
+
+def scalaNondeterministicSimulates(a: FSet.fset[((Nat.nat, Nat.nat), Transition.transition_ext[Unit])],
+                                   b: FSet.fset[((Nat.nat, Nat.nat), Transition.transition_ext[Unit])],
+                                   c: Nat.nat => Nat.nat): Boolean = false
