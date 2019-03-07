6ac10266df565d11356b68a32c16a4684cffbcc0
Ramsay Taylor <r.g.taylor@sheffield.ac.uk>
2019-03-07 18:08:21 +0000

    end of day 07/03/19

---
+++ formal/SAL-examples/CoinChoc.thy
@@ -0,0 +1,42 @@
+theory CoinChoc
+imports EFSM_LTL
+begin
+
+
+definition init :: transition where
+"init \<equiv> \<lparr>
+        Label = (STR ''init''),
+        Arity = 0,
+        Guard = [],
+        Outputs = [],
+        Updates = [(R 1, (L (Num 0)))]
+      \<rparr>"
+
+definition coin :: transition where
+"coin \<equiv> \<lparr>
+        Label = (STR ''coin''),
+        Arity = 0,
+        Guard = [],
+        Outputs = [],
+        Updates = [(R 1, (Plus (V (R 1)) (L (Num 1))))]
+      \<rparr>"
+
+definition vend :: transition where
+"vend \<equiv> \<lparr>
+        Label = (STR ''coin''),
+        Arity = 0,
+        Guard = [Ge (V (R 1)) (L (Num 0))],
+        Outputs = [],
+        Updates = [(R 1, (Minus (V (R 1)) (L (Num 1))))]
+      \<rparr>"
+
+definition drinks :: "transition_matrix" where
+"drinks \<equiv> {|
+          ((0,1), init),
+          ((1,1), coin),
+          ((1,2), vend)
+          |}"
+
+theorem must_pay: "not label=vend Until label=coin"
+
+end
