9f703be62a8e0b4dccd76bcf1b0b1850916e9ae4
Michael Foster <jmafoster1@sheffield.ac.uk>
2019-05-30 15:47:11 +0100

    Added the SAL stuff back in

--- inference-tool/src/main/scala/inference/Dirties.scala
+++ inference-tool/src/main/scala/inference/Dirties.scala
@@ -134,18 +134,16 @@ object Dirties {
   }
 
   def initiallyUndefinedContextCheck(e: iEFSM, r: Nat.nat, s: Nat.nat): Boolean = {
-    // TODO: Fix the exception thrown here
-    // val f = "intermediate_"+System.currentTimeMillis()
-    // TypeConversion.efsmToSALTranslator(Inference.tm(e), f)
-    //
-    // addLTL("salfiles/" + f + ".sal", s"  initiallyUndefined: THEOREM MichaelsEFSM |- G(cfstate = State_${valueOf(s)} => r_${valueOf(r)} = value_option ! None);")
-    //
-    // val output = Seq("bash", "-c", "cd salfiles; sal-smc --assertion='" + f + "{100}!initiallyUndefined'").!!
-    // if (output.toString != "proved.\n") {
-    //   print(output)
-    // }
-    // return (output.toString == "proved.\n")
-    return true
+    val f = "intermediate_"+System.currentTimeMillis()
+    TypeConversion.efsmToSALTranslator(Inference.tm(e), f)
+
+    addLTL("salfiles/" + f + ".sal", s"  initiallyUndefined: THEOREM MichaelsEFSM |- G(cfstate = State_${valueOf(s)} => r_${valueOf(r)} = value_option ! None);")
+
+    val output = Seq("bash", "-c", "cd salfiles; sal-smc --assertion='" + f + "{100}!initiallyUndefined'").!!
+    if (output.toString != "proved.\n") {
+      print(output)
+    }
+    return (output.toString == "proved.\n")
   }
 
   def salValue(v: Value.value): String = v match {
@@ -154,31 +152,30 @@ object Dirties {
   }
 
   def generaliseOutputContextCheck(l: String, ePrime: iEFSM, e: iEFSM, r: Nat.nat, v: Value.value, s_old: Nat.nat, s_new: Nat.nat): Boolean = {
-    // val f_old = "orig_"+System.currentTimeMillis()
-    // TypeConversion.efsmToSALTranslator(Inference.tm(e), f_old)
-    // val inxLabel = Code_Generation.input_updates_register(ePrime)
-    // addLTL("salfiles/" + f_old + s".sal", s"  inputValue: THEOREM MichaelsEFSM |-\n" +
-    //   s"    U(cfstate /= NULL_STATE, cfstate=State_${valueOf(s_old)}) =>\n"+
-    //   s"    U(label = ${inxLabel._2} => I(1) = ${salValue(v)}, X(cfstate = NULL_STATE));")
-    //
-    // val f_new = "intermediate_"+System.currentTimeMillis()
-    // TypeConversion.efsmToSALTranslator(Inference.tm(ePrime), f_new)
-    // addLTL("salfiles/" + f_new + ".sal", s"  generaliseOutput: THEOREM MichaelsEFSM |-\n"+
-    //   s"    U(cfstate /= NULL_STATE, cfstate = State_${valueOf(s_new)}) =>\n"+
-    //   s"    U(label = select => I(${valueOf(inxLabel._1)}) = ${salValue(v)}, cfstate = NULL_STATE) =>\n"+
-    //   s"    U(cfstate = State_${valueOf(s_new)} => r_1 = Some(${salValue(v)}), cfstate=NULL_STATE);")
-    //
-    //
-    // println(s"sal-smc --assertion='${f_old}{100}!inputValue'")
-    // val output1 = Seq("bash", "-c", s"cd salfiles; sal-smc --assertion='${f_old}{100}!inputValue'").!!
-    // println(output1)
-    //
-    // println(s"sal-smc --assertion='${f_new}{100}!generaliseOutput'")
-    // val output2 = Seq("bash", "-c", s"cd salfiles; sal-smc --assertion='${f_new}{100}!generaliseOutput'").!!
-    // println(output2)
-    //
-    // return (output1.toString == "proved.\n" && output2.toString == "proved.\n")
-    return true
+    val f_old = "orig_"+System.currentTimeMillis()
+    TypeConversion.efsmToSALTranslator(Inference.tm(e), f_old)
+    val inxLabel = Code_Generation.input_updates_register(ePrime)
+    addLTL("salfiles/" + f_old + s".sal", s"  inputValue: THEOREM MichaelsEFSM |-\n" +
+      s"    U(cfstate /= NULL_STATE, cfstate=State_${valueOf(s_old)}) =>\n"+
+      s"    U(label = ${inxLabel._2} => I(1) = ${salValue(v)}, X(cfstate = NULL_STATE));")
+
+    val f_new = "intermediate_"+System.currentTimeMillis()
+    TypeConversion.efsmToSALTranslator(Inference.tm(ePrime), f_new)
+    addLTL("salfiles/" + f_new + ".sal", s"  generaliseOutput: THEOREM MichaelsEFSM |-\n"+
+      s"    U(cfstate /= NULL_STATE, cfstate = State_${valueOf(s_new)}) =>\n"+
+      s"    U(label = select => I(${valueOf(inxLabel._1)}) = ${salValue(v)}, cfstate = NULL_STATE) =>\n"+
+      s"    U(cfstate = State_${valueOf(s_new)} => r_1 = Some(${salValue(v)}), cfstate=NULL_STATE);")
+
+
+    println(s"sal-smc --assertion='${f_old}{100}!inputValue'")
+    val output1 = Seq("bash", "-c", s"cd salfiles; sal-smc --assertion='${f_old}{100}!inputValue'").!!
+    println(output1)
+
+    println(s"sal-smc --assertion='${f_new}{100}!generaliseOutput'")
+    val output2 = Seq("bash", "-c", s"cd salfiles; sal-smc --assertion='${f_new}{100}!generaliseOutput'").!!
+    println(output2)
+
+    return (output1.toString == "proved.\n" && output2.toString == "proved.\n")
   }
 
   def scalaDirectlySubsumes(a: iEFSM,
