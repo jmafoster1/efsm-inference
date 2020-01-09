908099b509e1eba8a84d860cf789871ffddc0fd7
Michael Foster <jmafoster1@sheffield.ac.uk>
2020-01-09 11:00:48 +0000

    modified sharc-run

--- inference-tool/sharc-run.sh
+++ inference-tool/sharc-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 module load apps/java
 cd Documents/efsm-inference/inference-tool
-rm dotfiles/* salfiles/*
-java -jar /home/michael/Documents/efsm-inference/inference-tool/target/scala-2.12/inference-tool-assembly-0.1.0-SNAPSHOT.jar -p 3 -u 45 -h same,ws --skip sample-traces/liftDoors2.json
+export LD_LIBRARY_PATH=/home/acp17jmf/z3/build
+rm -r dotfiles/* salfiles/*
+java -jar target/scala-2.12/inference-tool-assembly-0.1.0-SNAPSHOT.jar -p 3 -u 45 -h same,ws --skip sample-traces/liftDoors2.json
