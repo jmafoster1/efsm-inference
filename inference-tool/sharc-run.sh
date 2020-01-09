b032c34dec0e43d84f7947e73f0513325ae8779b
Michael Foster <jmafoster1@sheffield.ac.uk>
2020-01-09 14:20:14 +0000

    long liftDoors2.json

--- inference-tool/sharc-run.sh
+++ inference-tool/sharc-run.sh
@@ -3,4 +3,4 @@ module load apps/java
 cd Documents/efsm-inference/inference-tool
 export LD_LIBRARY_PATH=/home/acp17jmf/z3/build
 rm -r dotfiles/* salfiles/*
-java -jar target/scala-2.12/inference-tool-assembly-0.1.0-SNAPSHOT.jar -p 3 -u 45 -h same,ws --skip sample-traces/liftDoors2.json
+~/jdk-11/bin/java -jar ~/inference-tool-assembly-0.1.0-SNAPSHOT.jar -p 3 -u 45 -h same,ws --skip sample-traces/liftDoors2.json
