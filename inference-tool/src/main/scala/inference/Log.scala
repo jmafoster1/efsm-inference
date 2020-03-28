59fd55ffbe0b8abcdf999d7442d8d8dc3d5db010
Michael Foster <jmafoster1@gmail.com>
2020-03-28 12:49:55 +0000

    end of day 28/03/20

--- inference-tool/src/main/scala/inference/Log.scala
+++ inference-tool/src/main/scala/inference/Log.scala
@@ -11,7 +11,7 @@ object Log {
   def logStates(e: Types.IEFSM, s_2: Nat.nat) = {
 		val s_1 = FSet.size_fset(Inference.S(e))
 		Config.numStates = Code_Numeral.integer_of_nat(s_1)
-		PrettyPrinter.iEFSM2dot(e, f"${PrettyPrinter.show(s_1)}-${PrettyPrinter.show(s_2)}")
+		PrettyPrinter.iEFSM2dot(e, f"${PrettyPrinter.show(s_2)}-${PrettyPrinter.show(s_1)}")
 		// println(Config.numStates)
     root.debug(s"${Code_Numeral.integer_of_nat(s_2)} -> ${Code_Numeral.integer_of_nat(s_1)}")
   }
