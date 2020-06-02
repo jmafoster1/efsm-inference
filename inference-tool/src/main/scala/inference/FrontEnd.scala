1a3a05828d75fabee2ebaef58ea6be2b6fc9a88d
Michael Foster <jmafoster1@gmail.com>
2020-06-02 16:26:10 +0100

    Testing the PTA

--- inference-tool/src/main/scala/inference/FrontEnd.scala
+++ inference-tool/src/main/scala/inference/FrontEnd.scala
@@ -15,6 +15,15 @@ object FrontEnd {
 
     var pta: IEFSM = Inference.make_pta(Config.config.train)
 
+    val eval = Inference.test_log(Config.config.test, pta)
+    val eval_json = s"""[\n  ${eval.map{
+      case (trace, rejected) => s"""{\n    "trace": [${if (trace.length > 0) "\n      " else ""}${trace.map(event => PrettyPrinter.to_JSON(event)).mkString(",\n      ")}${if (trace.length > 0) "\n    " else ""}],\n    "rejected": [${if (rejected.length > 0) "\n      " else ""}${rejected.map(event => PrettyPrinter.to_JSON(event)).mkString(",\n      ")}${if (rejected.length > 0) "\n    " else ""}]\n  }"""}.mkString(",\n  ")}\n]"""
+
+    val file = new File(Config.config.dotfiles + "/ptaLog.json")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(eval_json)
+    bw.close()
+
     PrettyPrinter.iEFSM2dot(pta, s"pta_gen")
 
     Config.numStates = Code_Numeral.integer_of_nat(FSet.size_fset(Inference.S(pta)))
