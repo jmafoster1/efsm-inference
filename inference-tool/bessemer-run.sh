47c699f3159358473d463cdb827ea2cc9288ed1b
jmafoster1 <jmafoster1@sheffield.ac.uk>
2020-06-07 14:05:30 +0100

    First two experimental runs

--- inference-tool/bessemer-run.sh
+++ inference-tool/bessemer-run.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #sBatch -c=4
 #SBATCH --mem=8000
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mail-user=jmafoster1@sheffield.ac.uk
 
 module load Java/11
@@ -11,4 +11,4 @@ module load Java/11
 echo "bash bessemer-run.sh "$@
 export LD_LIBRARY_PATH=/home/acp17jmf/z3/build
 
-java -jar target/scala-2.12/inference-tool-assembly-0.1.0-SNAPSHOT.jar --mkdir -s naive_eq_bonus -p $5 -g $1 -o $2 -u $3 -h ws,distinguish -d results/$4/$6/$5/$4-$5-$1-$2-$3 experimental-data/$4/$4-$6/$4-train.json experimental-data/$4/$4-$6/$4-test.json
+java -jar target/scala-2.12/inference-tool-assembly-0.1.0-SNAPSHOT.jar -s naive_eq_bonus -p $5 -g $1 -o $2 -u $3 -h ws -d results/$4/$6/$5/$4-$5-$1-$2-$3 experimental-data/$4/$4-$6/$4-train.json experimental-data/$4/$4-$6/$4-test.json
