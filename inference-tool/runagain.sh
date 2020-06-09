fb55fff6938addc3ab82688591944b399037842f
Michael Foster <jmafoster1@gmail.com>
2020-06-09 14:48:40 +0100

    Fixed graph drawer for new results file structure

--- inference-tool/runagain.sh
+++ inference-tool/runagain.sh
@@ -1,9 +1,9 @@
-rm results/spaceInvaders30/25/gp/spaceInvaders30-gp-15755-58765-22212; sbatch bessemer-run.sh 15755 58765 22212 spaceInvaders30 gp 25
-rm results/spaceInvaders30/28/gp/spaceInvaders30-gp-70105-24893-47815; sbatch bessemer-run.sh 70105 24893 47815 spaceInvaders30 gp 28
-rm results/spaceInvaders30/12/gp/spaceInvaders30-gp-28706-91177-41354; sbatch bessemer-run.sh 28706 91177 41354 spaceInvaders30 gp 12
-rm results/spaceInvaders30/28/gp/spaceInvaders30-gp-62647-88958-91277; sbatch bessemer-run.sh 62647 88958 91277 spaceInvaders30 gp 28
-rm results/spaceInvaders30/8/gp/spaceInvaders30-gp-94294-81224-78433; sbatch bessemer-run.sh 94294 81224 78433 spaceInvaders30 gp 8
-rm results/spaceInvaders30/29/gp/spaceInvaders30-gp-60687-90298-48634; sbatch bessemer-run.sh 60687 90298 48634 spaceInvaders30 gp 29
-rm results/spaceInvaders30/11/gp/spaceInvaders30-gp-20963-98449-14647; sbatch bessemer-run.sh 20963 98449 14647 spaceInvaders30 gp 11
-rm results/liftDoors30-obfuscated-time/12/gp/liftDoors30-obfuscated-time-gp-62647-88958-91277; sbatch bessemer-run.sh 62647 88958 91277 liftDoors30-obfuscated-time gp 12
-rm results/liftDoors30-obfuscated-time/12/gp/liftDoors30-obfuscated-time-gp-70639-16375-21118; sbatch bessemer-run.sh 70639 16375 21118 liftDoors30-obfuscated-time gp 12
+rm -r results/spaceInvaders30/25/gp/spaceInvaders30-gp-15755-58765-22212; sbatch bessemer-run.sh 15755 58765 22212 spaceInvaders30 gp 25
+rm -r results/spaceInvaders30/28/gp/spaceInvaders30-gp-70105-24893-47815; sbatch bessemer-run.sh 70105 24893 47815 spaceInvaders30 gp 28
+rm -r results/spaceInvaders30/12/gp/spaceInvaders30-gp-28706-91177-41354; sbatch bessemer-run.sh 28706 91177 41354 spaceInvaders30 gp 12
+rm -r results/spaceInvaders30/28/gp/spaceInvaders30-gp-62647-88958-91277; sbatch bessemer-run.sh 62647 88958 91277 spaceInvaders30 gp 28
+rm -r results/spaceInvaders30/8/gp/spaceInvaders30-gp-94294-81224-78433; sbatch bessemer-run.sh 94294 81224 78433 spaceInvaders30 gp 8
+rm -r results/spaceInvaders30/29/gp/spaceInvaders30-gp-60687-90298-48634; sbatch bessemer-run.sh 60687 90298 48634 spaceInvaders30 gp 29
+rm -r results/spaceInvaders30/11/gp/spaceInvaders30-gp-20963-98449-14647; sbatch bessemer-run.sh 20963 98449 14647 spaceInvaders30 gp 11
+rm -r results/liftDoors30/12/gp/liftDoors30-obfuscated-time-gp-62647-88958-91277; sbatch bessemer-run.sh 62647 88958 91277 liftDoors30-obfuscated-time gp 12
+rm -r results/liftDoors30/12/gp/liftDoors30-obfuscated-time-gp-70639-16375-21118; sbatch bessemer-run.sh 70639 16375 21118 liftDoors30-obfuscated-time gp 12
