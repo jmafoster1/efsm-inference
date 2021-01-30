b36015c81a1f887a90b8db55b3eabfa6ade61aee
Michael Foster <jmafoster1@gmail.com>
2021-01-30 11:13:38 +0000

    Moved submission scripts to submissions directory

---
+++ inference-tool/submissions/extraruns.sh
@@ -0,0 +1,149 @@
+# 90
+# bash experimental-data/spaceInvaders30/spaceInvaders30-1/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-1/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-1/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# 180
+# bash experimental-data/spaceInvaders30/spaceInvaders30-10/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-10/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-10/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #270
+# bash experimental-data/spaceInvaders30/spaceInvaders30-11/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-11/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-11/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #360
+# bash experimental-data/spaceInvaders30/spaceInvaders30-12/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-12/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-12/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #450
+# bash experimental-data/spaceInvaders30/spaceInvaders30-13/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-13/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-13/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #540
+# bash experimental-data/spaceInvaders30/spaceInvaders30-14/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-14/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-14/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #630
+# bash experimental-data/spaceInvaders30/spaceInvaders30-15/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-15/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-15/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #720
+# bash experimental-data/spaceInvaders30/spaceInvaders30-16/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-16/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-16/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #810
+# bash experimental-data/spaceInvaders30/spaceInvaders30-17/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-17/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-17/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #900
+# bash experimental-data/spaceInvaders30/spaceInvaders30-18/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-18/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-18/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+# #990
+# bash experimental-data/spaceInvaders30/spaceInvaders30-19/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-19/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-19/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #1080
+# bash experimental-data/spaceInvaders30/spaceInvaders30-2/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-2/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-2/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #1170
+# bash experimental-data/spaceInvaders30/spaceInvaders30-20/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-20/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-20/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+#1260
+# bash experimental-data/spaceInvaders30/spaceInvaders30-21/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-21/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-21/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1350
+# bash experimental-data/spaceInvaders30/spaceInvaders30-22/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-22/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-22/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1440
+# bash experimental-data/spaceInvaders30/spaceInvaders30-23/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-23/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-23/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1530
+# bash experimental-data/spaceInvaders30/spaceInvaders30-24/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-24/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-24/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1620
+# bash experimental-data/spaceInvaders30/spaceInvaders30-25/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-25/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-25/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1710
+# bash experimental-data/spaceInvaders30/spaceInvaders30-26/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-26/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-26/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1800
+# bash experimental-data/spaceInvaders30/spaceInvaders30-27/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-27/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-27/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1890
+# bash experimental-data/spaceInvaders30/spaceInvaders30-28/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-28/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-28/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#1980
+# bash experimental-data/spaceInvaders30/spaceInvaders30-29/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-29/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-29/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #2070
+# bash experimental-data/spaceInvaders30/spaceInvaders30-3/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-3/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-3/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+#
+# #2160
+# bash experimental-data/spaceInvaders30/spaceInvaders30-30/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-30/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-30/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#2250
+# bash experimental-data/spaceInvaders30/spaceInvaders30-4/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-4/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-4/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#2340
+# bash experimental-data/spaceInvaders30/spaceInvaders30-5/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-5/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-5/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#2430
+# bash experimental-data/spaceInvaders30/spaceInvaders30-6/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-6/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-6/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#2520
+# bash experimental-data/spaceInvaders30/spaceInvaders30-7/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-7/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-7/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#2610
+bash experimental-data/spaceInvaders30/spaceInvaders30-8/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+bash experimental-data/spaceInvaders30/spaceInvaders30-8/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+bash experimental-data/spaceInvaders30/spaceInvaders30-8/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
+
+#2700
+# bash experimental-data/spaceInvaders30/spaceInvaders30-9/spaceInvaders30-obfuscated-aliens-shields-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-9/spaceInvaders30-obfuscated-aliens-x-submissions-gp.sh
+# bash experimental-data/spaceInvaders30/spaceInvaders30-9/spaceInvaders30-obfuscated-x-shields-submissions-gp.sh
